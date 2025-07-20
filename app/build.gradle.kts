8ece2f3106c4d4c27f26133f6d29f4de3ac50d78
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-07-20 20:13:26 -0300

    chore: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Dependências de teste
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
