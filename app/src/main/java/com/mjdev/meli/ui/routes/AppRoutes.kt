9d4f9e0ca228abbf02fb91fd72c11e9ff688b0f5
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-07-21 15:44:26 -0300

    docs: documentation of AppRoutes

--- app/src/main/java/com/mjdev/meli/ui/routes/AppRoutes.kt
+++ app/src/main/java/com/mjdev/meli/ui/routes/AppRoutes.kt
@@ -2,14 +2,35 @@ package com.mjdev.meli.ui.routes
 
 import kotlinx.serialization.Serializable
 
+/**
+ * AppRoutes define as rotas do aplicativo, que são usadas para navegar entre diferentes telas.
+ * Cada rota é representada por um objeto serializável.
+ */
+
+/**
+ * SplashScreen
+ */
 @Serializable
 data object SplashRoute
 
+/**
+ * SearchScreen
+ */
 @Serializable
 data object SearchRoute
 
+/**
+ * ResultScreen
+ *
+ * @param searchQuery Texto de consulta usado para buscar produtos.
+ */
 @Serializable
 data class ResultRoute(val searchQuery: String)
 
+/**
+ * ProductDetailScreen
+ *
+ * @param productId ID do produto para exibir detalhes.
+ */
 @Serializable
 data class ProductDetailRoute(val productId: String)
