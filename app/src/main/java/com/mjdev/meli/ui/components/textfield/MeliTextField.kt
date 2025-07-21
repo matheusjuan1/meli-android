77f65ee4a554df0fa477c4b9e7238356910ce32c
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-07-21 15:33:27 -0300

    feat: add MeliTextField component

---
+++ app/src/main/java/com/mjdev/meli/ui/components/textfield/MeliTextField.kt
@@ -0,0 +1,65 @@
+package com.mjdev.meli.ui.components.textfield
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mjdev.meli.ui.theme.Typography
+
+/**
+ * MeliTextField é um componente de campo de texto reutilizável com suporte a foco e mudança de valor.
+ *
+ * @param value O valor atual do campo de texto.
+ * @param placeHolder Texto de espaço reservado exibido quando o campo está vazio.
+ * @param enabled Indica se o campo de texto está habilitado. Padrão é `true`.
+ * @param onValueChange A ação a ser executada quando o valor do campo de texto muda.
+ */
+@Composable
+fun MeliTextField(
+    value: String,
+    placeHolder: String = "",
+    enabled: Boolean = true,
+    onValueChange: (String) -> Unit
+) {
+
+    val focusRequester = remember { FocusRequester() }
+    var isFocused by remember { mutableStateOf(false) }
+
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester)
+            .onFocusChanged { isFocused = it.isFocused },
+        shape = RoundedCornerShape(14.dp),
+        value = value,
+        placeholder = { Text(placeHolder, style = Typography.labelMedium.copy(fontSize = 16.sp)) },
+        enabled = enabled,
+        textStyle = Typography.bodyMedium,
+        onValueChange = {
+            onValueChange(it)
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun MeliTextFieldPreview() {
+    MeliTextField(
+        value = "",
+        placeHolder = "Digite aqui",
+        onValueChange = { }
+    )
+}
