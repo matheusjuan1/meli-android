121354803a55ffd15d33dcd4b7a8119c37405cd8
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-07-22 17:22:34 -0300

    chore: add koin on project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.bundles.koin)
 
     // Dependências de teste
     testImplementation(libs.junit)
