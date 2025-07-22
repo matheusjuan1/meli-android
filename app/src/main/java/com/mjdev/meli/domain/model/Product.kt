6fc470c4febd5611ba957848f2a3e7ba9943b636
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-07-22 14:33:15 -0300

    feat: add properties in product model

--- app/src/main/java/com/mjdev/meli/domain/model/Product.kt
+++ app/src/main/java/com/mjdev/meli/domain/model/Product.kt
@@ -8,15 +8,19 @@ import java.math.BigDecimal
  * @property id ID único do produto.
  * @property title Título do produto.
  * @property price Preço do produto.
+ * @property originalPrice Preço original do produto, se disponível.
+ * @property permalink URL do produto no Mercado Livre.
  * @property currencyId ID da moeda do preço do produto.
+ * @property officialStoreName Nome da loja oficial do produto, se aplicável.
  * @property imageUrl URL da imagem do produto.
- * @property isFreeShipping Indica se o produto tem frete grátis.
  */
 data class Product(
     val id: String,
     val title: String,
     val price: BigDecimal,
+    val originalPrice: BigDecimal? = null,
+    val permalink: String,
     val currencyId: String,
+    val officialStoreName: String,
     val imageUrl: String,
-    val isFreeShipping: Boolean
 )
