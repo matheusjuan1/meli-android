b87c6930322263f6b43e47b191465735411e4391
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-07-24 01:21:41 -0300

    docs: add docs comments in ProductDetails

--- app/src/main/java/com/mjdev/meli/domain/model/ProductDetails.kt
+++ app/src/main/java/com/mjdev/meli/domain/model/ProductDetails.kt
@@ -2,7 +2,20 @@ package com.mjdev.meli.domain.model
 
 import java.math.BigDecimal
 
-
+/**
+ * ProductDetails representa os detalhes de um produto do Mercado Livre.
+ *
+ * @property id ID único do produto.
+ * @property title Título do produto.
+ * @property price Preço atual do produto.
+ * @property originalPrice Preço original do produto, se houver desconto.
+ * @property currencyId ID da moeda
+ * @property imageUrl URL da imagem do produto.
+ * @property permalink URL do produto na plataforma.
+ * @property warranty Garantia do produto, se disponível.
+ * @property condition Condição do produto (novo, usado, etc.).
+ * @property availableQuantity Quantidade disponível do produto em estoque.
+ */
 data class ProductDetails(
     val id: String,
     val title: String,
